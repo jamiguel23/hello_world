da771c8bc434639b0bf0baed734268900e853810
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2024-03-09 15:27:51 -0800

    while loop

--- hello_world.go
+++ hello_world.go
@@ -4,25 +4,11 @@ import (
 	"fmt"
 )
 
-func average(x []float64) (avg float64) {
-
-	total := 0.0
-	switch len(x) {
-	case 0:
-		avg = 0
-	default:
-		for _, v := range x {
-			total += v
-		}
-		avg = total / float64(len(x))
-	}
-
-	return
-}
 func main() {
 
-	// x := []float64{2.32, 23.42, 42.3, 29, 5}
-	z := []float64{}
-
-	fmt.Println(average((z)))
+	count := 1
+	for count < 5 {
+		count += count
+	}
+	fmt.Println(count)
 }
