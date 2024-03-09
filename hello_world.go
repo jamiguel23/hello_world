949d2121573004afbdc6afb823e3eb357aacc8d7
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2024-03-09 15:05:56 -0800

    maps' '

--- hello_world.go
+++ hello_world.go
@@ -2,40 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
 
-	languages := [9]string{
-		"C", "Lisp", "C++", "Java", "Python",
-		"JavaScript", "Ruby", "Go", "Rust", // Must include the trailing comma
-
+	firstReleases := map[string]int{
+		"C": 1972, "C++": 1985, "Java": 1996,
+		"Python": 1991, "JavaScript": 1996, "Go": 2012,
 	}
 
-	// define slices
-	/*
-		classics := languages[0:3]
-		// modern := make([]string, 4)
-		modern := languages[3:7]
-		new := languages[7:9]
-
-		fmt.Printf("the classics %v\n", classics)
-		fmt.Printf("Basic bois %v\n", modern)
-		fmt.Printf("new kids on the block%v\n", new)
-	*/
-
-	allLangs := languages[:]
-	fmt.Println(reflect.TypeOf(allLangs).Kind())
-
-	frameworks := []string{
-		"React", "Vue", "Angular", "Svelte",
-		"Laravel", "Django", "Flask", "Fiber",
+	for k, v := range firstReleases {
+		fmt.Printf("%s was fiesrt released in %d\n", k, v)
 	}
-
-	jsFrameworks := frameworks[0:4:4]
-	frameworks = append(frameworks, "meteor")
-
-	fmt.Printf("all frameworks %v\n", frameworks)
-	fmt.Printf("js frameworks %v\n", jsFrameworks)
 }
