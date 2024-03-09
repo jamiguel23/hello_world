16a646a1d544737d15078b6cb609ef259e4f3839
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2024-03-09 15:24:42 -0800

    switch statement

--- hello_world.go
+++ hello_world.go
@@ -6,18 +6,14 @@ import (
 
 func average(x []float64) (avg float64) {
 
-	// initialize floating variable to 0
 	total := 0.0
-	// check if length is 0
-	if len(x) == 0 {
+	switch len(x) {
+	case 0:
 		avg = 0
-	} else {
-		// for loop to iterate through the range of the array
+	default:
 		for _, v := range x {
-			// add float to total
 			total += v
 		}
-		// divide total with lengh of the array but as a float64 (the same data type)
 		avg = total / float64(len(x))
 	}
 
