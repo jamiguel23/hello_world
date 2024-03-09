4d0e754a655aa44074a9139cbf240883040002f1
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2024-03-09 15:18:42 -0800

    average for loop

--- hello_world.go
+++ hello_world.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 )
 
+func average(x []float64) (avg float64) {
+
+	// initialize floating variable to 0
+	total := 0.0
+	// check if length is 0
+	if len(x) == 0 {
+		avg = 0
+	} else {
+		// for loop to iterate through the range of the array
+		for _, v := range x {
+			// add float to total
+			total += v
+		}
+		// divide total with lengh of the array but as a float64 (the same data type)
+		avg = total / float64(len(x))
+	}
+
+	return
+}
 func main() {
 
-	firstReleases := map[string]int{
-		"C": 1972, "C++": 1985, "Java": 1996,
-		"Python": 1991, "JavaScript": 1996, "Go": 2012,
-	}
+	// x := []float64{2.32, 23.42, 42.3, 29, 5}
+	z := []float64{}
 
-	for k, v := range firstReleases {
-		fmt.Printf("%s was fiesrt released in %d\n", k, v)
-	}
+	fmt.Println(average((z)))
 }
